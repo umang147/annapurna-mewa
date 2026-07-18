c335779374073edde235bd592468959882bc57d9
Umang Chhaparia <umang147@gmail.com>
2026-07-18 16:19:38 +0530

    fix: redirect vercel domain to canonical site

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'annapurna-mewa.vercel.app',
+          },
+        ],
+        destination: 'https://annapurnamewa.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
